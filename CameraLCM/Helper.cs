21bb9a74c2a8e122b611572ea979cf73a0c7ba2e
Jeffrey Lipton <jeffreyilipton@gmail.com>
2017-04-06 17:08:55 -0400

    started webcam to LCM program

---
+++ CameraLCM/Helper.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Windows.Media.Imaging;
+using LCM.LCM;
+
+namespace WPFCSharpWebCam
+{
+    //Design by Pongsakorn Poosankam
+    class Helper
+    {
+        //Block Memory Leak
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        public static extern bool DeleteObject(IntPtr handle);
+        public static BitmapSource bs;
+        public static IntPtr ip;
+        public static BitmapSource LoadBitmap(System.Drawing.Bitmap source)
+        {
+
+            ip = source.GetHbitmap();
+
+            bs = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(ip, IntPtr.Zero, System.Windows.Int32Rect.Empty,
+
+                System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+
+            DeleteObject(ip);
+
+            return bs;
+
+        }
+        public static void SaveImageCapture(BitmapSource bitmap)
+        {
+            JpegBitmapEncoder encoder = new JpegBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(bitmap));
+            encoder.QualityLevel = 100;
+
+
+            // Configure save file dialog box
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = "Image"; // Default file name
+            dlg.DefaultExt = ".Jpg"; // Default file extension
+            dlg.Filter = "Image (.jpg)|*.jpg"; // Filter files by extension
+
+            // Show save file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process save file dialog box results
+            if (result == true)
+            {
+                // Save Image
+                string filename = dlg.FileName;
+                FileStream fstream = new FileStream(filename, FileMode.Create);
+                encoder.Save(fstream);
+                fstream.Close();
+            }
+
+        }
+        public static byte[] BufferFromImage(BitmapImage imageSource)
+        {
+            Stream stream = imageSource.StreamSource;
+            Byte[] buffer = null;
+            if (stream != null && stream.Length > 0)
+            {
+                using (BinaryReader br = new BinaryReader(stream))
+                {
+                    buffer = br.ReadBytes((Int32)stream.Length);
+                }
+            }
+
+            return buffer;
+        }
+
+        public static byte[] ReadImageMemory(BitmapSource bitmapSource)
+        {
+            JpegBitmapEncoder encoder = new JpegBitmapEncoder();
+            MemoryStream memoryStream = new MemoryStream();
+            encoder.Frames.Add(BitmapFrame.Create(bitmapSource));
+            encoder.Save(memoryStream);
+            return memoryStream.GetBuffer();
+        }
+    }
+}
---
+++ CameraLCM/Window1.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using LCM.LCM;
+
+
+namespace WPFCSharpWebCam
+{
+    /// <summary>
+    /// Design by Pongsakorn Poosankam
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        static public LCM.LCM.LCM myLCM;
+        public Window1()
+        {
+            InitializeComponent();
+            
+        }
+
+        WebCam webcam;
+        private String channel;
+        private System.Timers.Timer aTimer = new System.Timers.Timer();
+        private bool started = false;
+
+        private void mainWindow_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	// TODO: Add event handler implementation here.
+            webcam = new WebCam();
+            webcam.InitializeWebCam(ref imgVideo);
+            myLCM = new LCM.LCM.LCM("udpm://239.255.76.67:7667:?ttl=1");
+            channel = "defaulImage";
+            aTimer.Elapsed += new System.Timers.ElapsedEventHandler(publish);
+            aTimer.Interval = 100;
+            aTimer.Enabled = true;
+        }
+
+        private void bntStart_Click(object sender, RoutedEventArgs e)
+        {
+            
+            webcam.Start();
+            started = true;
+        }
+
+        private void bntStop_Click(object sender, RoutedEventArgs e)
+        {
+            webcam.Stop();
+            started = false;
+        }
+
+        private void bntContinue_Click(object sender, RoutedEventArgs e)
+        {
+            webcam.Continue();
+            started = true;
+        }
+
+        private void bntCapture_Click(object sender, RoutedEventArgs e)
+        {
+            imgCapture.Source = imgVideo.Source;
+        }
+
+        private void bntSaveImage_Click(object sender, RoutedEventArgs e)
+        {
+            Helper.SaveImageCapture((BitmapSource)imgCapture.Source);
+        }
+
+        private void bntResolution_Click(object sender, RoutedEventArgs e)
+        {
+            webcam.ResolutionSetting();
+        }
+
+        private void bntSetting_Click(object sender, RoutedEventArgs e)
+        {
+            webcam.AdvanceSetting();
+        }
+
+        private void publish(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            publish();
+        }
+        private void publish() { 
+            if (started == true && channel != "")
+            {
+                oculuslcm.image_t frame = new oculuslcm.image_t();
+                imgCapture.Source = imgVideo.Source;
+                BitmapSource BS = (BitmapSource)imgCapture.Source;
+
+                frame.width = BS.PixelWidth;
+                frame.height = BS.PixelHeight;
+                frame.data = Helper.ReadImageMemory(BS);
+                myLCM.Publish(channel, frame);
+            }
+
+        }
+
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            channel = comboBox.Text;
+        }
+
+    }
+}
