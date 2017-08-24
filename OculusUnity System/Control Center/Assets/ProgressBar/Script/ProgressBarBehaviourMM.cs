9d538daf36a120e651649b956688b8d5a3b678bf
jeffrey lipton <jlipton@mit.edu>
2017-08-24 16:35:50 -0400

    Working Range

--- OculusUnity System/Control Center/Assets/ProgressBar/Script/ProgressBarBehaviourMM.cs
+++ OculusUnity System/Control Center/Assets/ProgressBar/Script/ProgressBarBehaviourMM.cs
@@ -13,12 +13,25 @@ namespace ProgressBar
         /// This method is used to set the Filler's width and display mm
         /// </summary>
         /// <param name="Width">the new Filler's width</param>
-        public override void SetFillerSize(float Width)
+        public override void SetFillerSize(float value)
         {
-            if (m_AttachedText)
-                m_AttachedText.text = Mathf.Round(Width / FillerInfo.MaxWidth * 100).ToString() + " mm";
 
-            m_FillRect.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, XOffset, Width);
+            if (value * 8 < FillerInfo.MaxWidth)
+            {
+                m_FillRect.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, XOffset, value * 8);
+                if (m_AttachedText)
+                {
+                    m_AttachedText.text = Mathf.Round(value / FillerInfo.MaxWidth * 100).ToString() + " mm";
+                }
+            }
+            else
+            {
+                m_FillRect.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, XOffset, FillerInfo.MaxWidth);
+                if (m_AttachedText)
+                {
+                    m_AttachedText.text = "Invalid";
+                }
+            }
         }
     }
 }
